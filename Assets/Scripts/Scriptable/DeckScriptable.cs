fa5cd535bdb46bde2f239d45056ca8963f33ff45
Marko Francuski <francmarko@yahoo.com>
2021-01-22 03:33:47 +0100

    Add MEC plugin, Deck WIP

--- Assets/Scripts/Scriptable/DeckScriptable.cs
+++ Assets/Scripts/Scriptable/DeckScriptable.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using MEC;
+using Random = UnityEngine.Random;
+using System;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "Deck", menuName = "Assets/Deck", order = 0)]
 public class DeckScriptable : ScriptableObject
@@ -30,7 +34,7 @@ public class DeckScriptable : ScriptableObject
     }
     public void RemoveCard(CardBaseScriptable cardToRemove)
     {
-        if(Cards.Count > 0)
+        if (Cards.Count > 0)
         {
             CardBaseScriptable removedCard = Cards.Dequeue();
             Debug.LogWarning($"Card Removed from the Deck");
@@ -38,7 +42,6 @@ public class DeckScriptable : ScriptableObject
         else
         {
             Debug.LogWarning($"Cannot Remove Card from the Deck");
-
         }
     }
     public CardBaseScriptable DrawCard()
@@ -57,7 +60,7 @@ public class DeckScriptable : ScriptableObject
     }
     public void UpdateCardsInDeck(List<CardBaseScriptable> cards)
     {
-        if(cards.Count > MaxCardsInDeck)
+        if (cards.Count > MaxCardsInDeck)
         {
             Debug.LogWarning($"Cannot Update Deck{DeckName}, max cards exceeded!");
             return;
@@ -67,10 +70,48 @@ public class DeckScriptable : ScriptableObject
 
     public void ShuffleDeck(int guarrantedPawn = 1)
     {
-        List<CardBaseScriptable> guarrantedPawns = new List<CardBaseScriptable>(guarrantedPawn);
-
-
-
+        Timing.RunCoroutine(_GetRandomFromDeckCoroutine(_cardsList, guarrantedPawn, GetRandomGuarantedPawns), "GuarantedPawnsCoroutine");
     }
 
+    private void GetRandomGuarantedPawns(List<int> indexes)
+    {
+        List<CardBaseScriptable> guarrantedPawns = new List<CardBaseScriptable>(indexes.Count);
+
+        foreach (int index in indexes)
+        {
+            guarrantedPawns.Add(_cardsList[index]);
+        }
+
+        //Save list or return it.
+    }
+
+
+    #region COROUTINES
+    private IEnumerator<float> _GetRandomFromDeckCoroutine(List<CardBaseScriptable> cards, int gurantedPawns, UnityAction<List<int>> returnCallback)
+    {
+        int currentPawns = 0;
+        List<int> alreadyTriedCards = new List<int>();
+        List<int> pawnIndexes = new List<int>();
+
+        while (gurantedPawns > currentPawns)
+        {
+            int randomIndex = Random.Range(0, cards.Count);
+
+            while (alreadyTriedCards.Contains(randomIndex))
+                randomIndex = Random.Range(0, cards.Count);
+
+            alreadyTriedCards.Add(randomIndex);
+
+            if ((int)cards[randomIndex].CardType <= 1) //Normal/Champion Card => (0,1);
+            {
+                currentPawns++;
+                pawnIndexes.Add(randomIndex);
+            }
+            yield return Timing.WaitForOneFrame;
+        }
+        returnCallback(pawnIndexes);
+    }
+    #endregion COROUTINES
 }
+
+
